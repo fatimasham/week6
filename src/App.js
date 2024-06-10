64dbacc5c764ea5988790b2b39c0f3db7e37ba6d
fatima shamsieva <diel1989@mail.ru>
2024-06-10 12:44:40 +0500

    created data.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
+
 function App() {
+
+  const [toDo, setToDo] = useState(data);
+  const removeItem = (id) => {
+
+  let newToDo = toDo.filter(toDo => toDo.id !== id)
+  setToDo(newToDo)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+    <div className="container">
+      <h1>The {toDo.length} Best Places To Visit In Paris</h1>
     </div>
-  );
+
+      {toDo.map((place => {
+      const {id, toVisit, image} = place;
+
+      return(<div key={id}>
+        <div className="container">
+          <h2>{id} - {toVisit}</h2>
+        </div>
+
+        <div className="container">
+          <img src={image} width="300px"/>
+        </div>
+
+        <div className="container">
+          <button onClick={() => removeItem(id)}>Remove</button>
+        </div>
+
+
+  </div>)
+      }))}
+      <div className="container">
+      <button onClick={() => setToDo([])}>Delete All</button>
+    </div>
+    </div>
+
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const data = [
+
+    {
+        id: 1,
+        toVisit: "Eiffel Tower",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20784_20770_1.800w.jpg?ver=1675418131"
+    },
+
+    {
+        id: 2,
+        toVisit: "Notre Dame Cathedral",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20792_0bd10b87f37ed3c3edf836e8b8f5a254.jpg?ver=1675418131"
+    },
+
+    {
+        id: 3,
+        toVisit: "Louvre Museum",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20842_e98a166f199a380b378efb6f017e3f03.jpg?ver=1675418131"
+    },
+
+    {
+        id: 4,
+        toVisit: "Champs Elysées / Arc of Triumph",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20851_d1d1fce4a3edf1bd0c2dd99efea42891.jpg?ver=1675418131"
+    },
+
+    {
+        id: 5,
+        toVisit: "Montmartre",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20869_9e3d664c1e25f1a8a16bd1bee9586891.jpg?ver=1675418131"
+    },
+
+    {
+        id: 6,
+        toVisit: "Palace of Versailles",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20878_30840ae13750c6db09b37665b0fcb9f2.jpg?ver=1675418131"
+    },
+
+    {
+        id: 7,
+        toVisit: "Moulin Rouge",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20896_0863c5af95f18a8cb78a34a2e00977a8.jpg?ver=1675418131"
+    },
+
+    {
+        id: 8,
+        toVisit: "Disneyland Paris",
+        image: "https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20905_208cfcc257a889002227594b5fd7a9ae.jpg?ver=1675418131"
+    }
+
+]
